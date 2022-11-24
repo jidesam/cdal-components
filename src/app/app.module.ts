48a7cd87679278277c5ae11d294b50c7280721db
Olajide <olajidesamson017@gmail.com>
2022-11-24 10:30:36 +0100

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputFieldComponent } from './input-field/input-field.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputFieldComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
