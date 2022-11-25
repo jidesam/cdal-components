4328aae17d6e9ddbff38ba2bdc48a53232f33f24
Olajide <olajidesamson017@gmail.com>
2022-11-25 11:46:51 +0100

    existing users update

--- src/app/main/existing-users/existing-users.component.ts
+++ src/app/main/existing-users/existing-users.component.ts
@@ -12,5 +12,44 @@ export class ExistingUsersComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  customerDetails = [
+    {
+      index: 1,
+      customerDetails : 'Adekunle Ibraheem',
+      date: 'April 28 2022',
+      statusOfRequest: 'Granted'
+    },
+     {
+      index: 2,
+      customerDetails : 'Adekunle Ibraheem',
+      date: 'April 28 2022',
+      statusOfRequest: 'Pending'
+    },
+     {
+      index: 3,
+      customerDetails : 'Adekunle Ibraheem',
+      date: 'April 28 2022',
+      statusOfRequest: 'Rejected'
+    },
+     {
+      index: 4,
+      customerDetails : 'Adekunle Ibraheem',
+      date: 'April 28 2022',
+      statusOfRequest: 'Pending'
+    },
+     {
+      index: 5,
+      customerDetails : 'Adekunle Ibraheem',
+      date: 'April 28 2022',
+      statusOfRequest: 'Granted'
+    },
+     {
+      index: 6,
+      customerDetails : 'Adekunle Ibraheem',
+      date: 'April 28 2022',
+      statusOfRequest: 'Granted'
+    },
+
+  ]
 
 }
