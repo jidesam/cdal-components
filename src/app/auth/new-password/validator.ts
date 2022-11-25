1dfe60a94fb2b89dd4a0fc4eaadc9946dd50ceb6
ennyitan <ololadedamilola7@gmail.com>
2022-11-25 13:11:58 +0100

    update

--- src/app/auth/new-password/validator.ts
+++ src/app/auth/new-password/validator.ts
@@ -2,10 +2,10 @@ import { AbstractControl } from "@angular/forms";
 
 export function PasswordValidator(control:AbstractControl): {[key:string]: boolean} | null {
 
-    const password = control.get('password')
+    const newPassword = control.get('newPassword')
     const confirmPassword = control.get('confirmPassword')
-    if(password?.pristine || confirmPassword?.pristine) {
+    if(newPassword?.pristine || confirmPassword?.pristine) {
         return null 
     }
-    return password && confirmPassword &&password.value != confirmPassword.value ?  {'misMatch' : true} : null
+    return newPassword && confirmPassword &&newPassword.value != confirmPassword.value ?  {'misMatch' : true} : null
 }
